acd8277bd5a468a1b79f3abd3bfdc487e904b9a3
madiwa Simon <mad.simon01@gmail.com>
2020-11-04 19:59:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,14 @@
-# don't forget to add: require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-
+binding.pry
+puts "hello"
 def engage
-  puts state_log(date)
+  puts state_log(star_date)
   date = generate_star_date
 end
