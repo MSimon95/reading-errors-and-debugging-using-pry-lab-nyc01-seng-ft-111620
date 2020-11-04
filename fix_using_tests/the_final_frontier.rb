876b2f5233637b63e937ddcab4ce6016ebcd4165
madiwa Simon <mad.simon01@gmail.com>
2020-11-04 20:15:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,7 +7,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(star_date)
+def engage
+   star_date = generate_star_date
   puts state_log(star_date)
-  star_date = generate_star_date
 end
